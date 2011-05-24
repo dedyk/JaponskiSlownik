c43f0e6f7a0380da0c74a62ec0e3563ba2d97809
Fryderyk Mazurek <fmazurek@idedyk.pl>
2011-05-24 22:39:03 +0200

    Small fix.

--- src/main/java/pl/idedyk/japanese/dictionary/japannaka/JapannakaJavaCsvGenerator.java
+++ src/main/java/pl/idedyk/japanese/dictionary/japannaka/JapannakaJavaCsvGenerator.java
@@ -18,19 +18,19 @@ public class JapannakaJavaCsvGenerator {
 		
 		for (PolishJapaneseEntry polishJapaneseEntry : japanesePolishDictionary) {
 			
-			sb.append(polishJapaneseEntry.getRomaji()).append(";");
-			sb.append(polishJapaneseEntry.getJapanese()).append(";");
+			sb.append(replaceChars(polishJapaneseEntry.getRomaji())).append(";");
+			//sb.append(polishJapaneseEntry.getJapanese()).append(";");
 			
 			List<PolishTranslate> polishTranslates = polishJapaneseEntry.getPolishTranslates();
 			
 			if (polishTranslates != null) {
 				for (PolishTranslate currentPolishTranslate : polishTranslates) {
 					
-					sb.append(currentPolishTranslate.getWord()).append("|");
+					sb.append(replaceChars(currentPolishTranslate.getWord())).append("|");
 					
 					if (currentPolishTranslate.getInfo() != null && currentPolishTranslate.getInfo().size() > 0) {
 						for (int infoIdx = 0; infoIdx < currentPolishTranslate.getInfo().size(); ++infoIdx) {
-							sb.append(currentPolishTranslate.getInfo().get(infoIdx));
+							sb.append(replaceChars(currentPolishTranslate.getInfo().get(infoIdx)));
 							
 							if (infoIdx != currentPolishTranslate.getInfo().size() - 1) {
 								sb.append(",");
@@ -46,5 +46,40 @@ public class JapannakaJavaCsvGenerator {
 		}
 		
 		System.out.println(sb.toString());
+	}
+	
+	private static String replaceChars(String text) {
+		
+		text = text.replaceAll("Ę", "E");
+		text = text.replaceAll("ę", "e");
+		
+		text = text.replaceAll("Ó", "O");
+		text = text.replaceAll("ó", "o");
+		
+		text = text.replaceAll("Ą", "A");
+		text = text.replaceAll("ą", "a");
+		
+		text = text.replaceAll("Ś", "S");
+		text = text.replaceAll("ś", "s");
+		
+		text = text.replaceAll("Ł", "L");
+		text = text.replaceAll("ł", "l");
+		
+		text = text.replaceAll("Ż", "Z");
+		text = text.replaceAll("ż", "z");
+		
+		text = text.replaceAll("Ź", "z");
+		text = text.replaceAll("ź", "z");
+		
+		text = text.replaceAll("Ć", "C");
+		text = text.replaceAll("ć", "c");
+		
+		text = text.replaceAll("Ń", "N");
+		text = text.replaceAll("ń", "n");
+		
+		text = text.replaceAll("ō", "o-");
+		text = text.replaceAll("ū", "u-");
+		
+		return text;
 	}
 }
