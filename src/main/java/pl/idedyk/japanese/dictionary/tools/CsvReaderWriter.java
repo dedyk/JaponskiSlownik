eca01260472289f4caedb42bd2b4bd6d4c2baa13
Fryderyk Mazurek <fmazurek@idedyk.pl>
2012-03-09 18:40:09 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/tools/CsvReaderWriter.java
+++ src/main/java/pl/idedyk/japanese/dictionary/tools/CsvReaderWriter.java
@@ -1,18 +1,23 @@
 package pl.idedyk.japanese.dictionary.tools;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
 
 import pl.idedyk.japanese.dictionary.dto.KanaEntry;
 import pl.idedyk.japanese.dictionary.dto.PolishJapaneseEntry;
 import pl.idedyk.japanese.dictionary.dto.PolishTranslate;
 import pl.idedyk.japanese.dictionary.dto.RomajiEntry;
+import pl.idedyk.japanese.dictionary.genki.DictionaryEntryType;
+import pl.idedyk.japanese.dictionary.genki.WordType;
 
-public class CsvGenerator {
+public class CsvReaderWriter {
 
 	public static void generateDictionaryApplicationResult(String outputFile, List<PolishJapaneseEntry> polishJapaneseEntries) throws IOException {
 		
@@ -43,7 +48,7 @@ public class CsvGenerator {
 			}
 			sb.append(";");				
 
-			if (polishJapaneseEntry.getJapanese() != null) {
+			if (polishJapaneseEntry.getJapanese() != null && polishJapaneseEntry.getJapanese().equals("") == false) {
 				sb.append(polishJapaneseEntry.getJapanese()).append(";");
 			}
 
@@ -98,6 +103,39 @@ public class CsvGenerator {
 		csvWriter.close();
 	}
 	
+	public static List<PolishJapaneseEntry> parsePolishJapaneseEntriesFromCsv(String fileName) throws IOException {
+		
+		List<PolishJapaneseEntry> result = new ArrayList<PolishJapaneseEntry>();
+		
+		CsvReader csvReader = new CsvReader(new FileReader(fileName), ',');
+		
+		while(csvReader.readRecord()) {
+			
+			String groupNameString = csvReader.get(0);
+			String wordTypeString = csvReader.get(1);
+			String japaneseString = csvReader.get(2);
+			String romajiListString = csvReader.get(3);
+			String polishTranslateListString = csvReader.get(4);
+			String infoString = csvReader.get(5);
+			
+			PolishJapaneseEntry entry = new PolishJapaneseEntry();
+			
+			entry.setGroupName(DictionaryEntryType.valueOf(groupNameString));
+			entry.setWordType(WordType.valueOf(wordTypeString));
+			entry.setJapanese(japaneseString);
+			entry.setRomajiList(parsePolishRomajiString(romajiListString));
+			entry.setPolishTranslates(parsePolishTranslateString(polishTranslateListString));
+			
+			entry.setInfo(infoString);
+			
+			result.add(entry);
+		}
+		
+		csvReader.close();
+		
+		return result;
+	}
+	
 	private static String convertPolishTranslateListToString(List<PolishTranslate> list) {
 		StringBuffer sb = new StringBuffer();
 		
@@ -111,7 +149,24 @@ public class CsvGenerator {
 		
 		return sb.toString();
 	}
-
+	
+	private static List<PolishTranslate> parsePolishTranslateString(String polishTranslateString) {
+		
+		List<PolishTranslate> result = new ArrayList<PolishTranslate>();
+		
+		String[] splitedPolishTranslateString = polishTranslateString.split("\n");
+		
+		for (String currentpolishTranslateString : splitedPolishTranslateString) {
+			PolishTranslate polishTranslate = new PolishTranslate();
+			
+			polishTranslate.setWord(currentpolishTranslateString);
+			
+			result.add(polishTranslate);
+		}
+		
+		return result;		
+	}
+	
 	private static String convertRomajiEntryListToString(List<RomajiEntry> list) {
 		StringBuffer sb = new StringBuffer();
 		
@@ -125,6 +180,24 @@ public class CsvGenerator {
 		
 		return sb.toString();
 	}
+	
+	private static List<RomajiEntry> parsePolishRomajiString(String romajiString) {
+		
+		List<RomajiEntry> result = new ArrayList<RomajiEntry>();
+		
+		String[] splitedRomajiListString = romajiString.split("\n");
+		
+		for (String currentRomajiString : splitedRomajiListString) {
+			RomajiEntry romajiEntry = new RomajiEntry();
+			
+			romajiEntry.setRomaji(currentRomajiString);
+			
+			result.add(romajiEntry);
+		}
+		
+		return result;		
+	}
+
 	
 	public static void generateKanaEntriesCsv(String outputFile, List<KanaEntry> kanaEntries) throws IOException {
 		
