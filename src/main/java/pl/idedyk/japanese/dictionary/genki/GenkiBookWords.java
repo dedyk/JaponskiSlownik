0809efa21aa4b3b2d3a95339a16c241701a7f872
Fryderyk Mazurek <fmazurek@idedyk.pl>
2012-03-09 15:26:27 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/genki/GenkiBookWords.java
+++ src/main/java/pl/idedyk/japanese/dictionary/genki/GenkiBookWords.java
@@ -1381,6 +1381,8 @@ public class GenkiBookWords {
 		
 		addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_10, WordType.HIRAGANA, "tsumori desu", "mam zamiar", "+ czasownik w formie prostej");
 		
+		addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_10, WordType.HIRAGANA, "aruite iku", "iść na piechotę", null);
+		
 		return result;
 	}
 	
@@ -1961,7 +1963,7 @@ public class GenkiBookWords {
 		addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_10, WordType.HIRAGANA, "朝食", new String[] { "choushoku" }, new String[] { "śniadanie" }, null);
 		
 		addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_10, WordType.HIRAGANA, "持つ", new String[] { "motsu" }, new String[] { "mieć" }, null);
-		addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_10, WordType.HIRAGANA, "持っている", new String[] { "motte kuru" }, new String[] { "przynosić" }, null);
+		addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_10, WordType.HIRAGANA, "持ってくる", new String[] { "motte kuru" }, new String[] { "przynosić" }, null);
 		addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_10, WordType.HIRAGANA, "所持品", new String[] { "shojihin" }, new String[] { "dobytek" }, null);
 		
 		//addPolishJapaneseEntry(polishJapaneseEntries, DictionaryEntryType.WORD_GENKI1_4, WordType.HIRAGANA, "kanji", "czytanie", "tlumaczenie", null);
