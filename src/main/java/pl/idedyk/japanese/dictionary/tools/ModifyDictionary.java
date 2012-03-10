082a2c25b5f7b22d8d9aad524d5d1cc3baf8322a
Fryderyk Mazurek <fmazurek@idedyk.pl>
2012-03-10 20:30:07 +0100

    update

---
+++ src/main/java/pl/idedyk/japanese/dictionary/tools/ModifyDictionary.java
@@ -0,0 +1,71 @@
+package pl.idedyk.japanese.dictionary.tools;
+
+import java.util.List;
+
+import pl.idedyk.japanese.dictionary.dto.PolishJapaneseEntry;
+import pl.idedyk.japanese.dictionary.genki.DictionaryEntryType;
+
+public class ModifyDictionary {
+
+	public static void main(String[] args) throws Exception {
+		
+		List<PolishJapaneseEntry> polishJapaneseEntries = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input/word.csv");
+		
+		for (PolishJapaneseEntry polishJapaneseEntry : polishJapaneseEntries) {
+			
+			String polishJapaneseInfo = polishJapaneseEntry.getInfo();
+			
+			if (polishJapaneseInfo != null && polishJapaneseInfo.equals("") == false) {
+				
+				DictionaryEntryType newDictionaryEntry = null;
+				
+				if (polishJapaneseInfo.indexOf("ru-czasownik") != -1) {
+					//System.out.println(polishJapaneseInfo);
+					
+					polishJapaneseInfo = polishJapaneseInfo.replaceAll("ru-czasownik", "");
+					
+					polishJapaneseInfo = fixInfo(polishJapaneseInfo);
+					checkInfo(polishJapaneseInfo);
+					
+					newDictionaryEntry = DictionaryEntryType.WORD_VERB_RU;
+				}
+				
+				
+				
+				
+				polishJapaneseEntry.setInfo(polishJapaneseInfo);
+				
+				if (newDictionaryEntry != null) {
+					polishJapaneseEntry.setDictionaryEntryType(newDictionaryEntry);
+				}
+			}
+		}
+		
+		CsvReaderWriter.generateCsv("input/word-temp.csv", polishJapaneseEntries);
+	}
+
+	private static String fixInfo(String polishJapaneseInfo) {
+		
+		polishJapaneseInfo = polishJapaneseInfo.trim();
+		
+		if (polishJapaneseInfo.startsWith(",") == true) {
+			polishJapaneseInfo = polishJapaneseInfo.substring(1);
+		}
+		
+		if (polishJapaneseInfo.endsWith(",") == true) {
+			polishJapaneseInfo = polishJapaneseInfo.substring(0, polishJapaneseInfo.length() - 1);
+		}
+		
+		polishJapaneseInfo = polishJapaneseInfo.trim();
+		
+		return polishJapaneseInfo;
+	}
+
+	private static void checkInfo(String polishJapaneseInfo) {
+		
+		if (polishJapaneseInfo.length() > 0) {
+			System.out.println(polishJapaneseInfo);
+		}
+		
+	}
+}
