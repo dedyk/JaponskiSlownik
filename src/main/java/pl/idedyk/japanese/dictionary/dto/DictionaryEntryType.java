ccdf1eaec06452f267b7204b29eb24d4c93ab3d7
dedyk <dedykjp@gmail.com>
2013-10-11 21:38:12 +0200

    update

--- src/main/java/pl/idedyk/japanese/dictionary/dto/DictionaryEntryType.java
+++ src/main/java/pl/idedyk/japanese/dictionary/dto/DictionaryEntryType.java
@@ -1,67 +1,73 @@
 package pl.idedyk.japanese.dictionary.dto;
 
 public enum DictionaryEntryType {
-	
+
 	WORD_GREETING("Powitanie"),
-		
+
 	WORD_NUMBER("Liczba"),
-	
+
 	WORD_TIME("Godzina"),
-	
+
 	WORD_AGE("Wiek"),
-	
+
 	WORD_NOUN("rzeczownik"),
-	
+
 	WORD_VERB_U("u-czasownik"),
-	
+
 	WORD_VERB_RU("ru-czasownik"),
-	
+
 	WORD_VERB_TE("te-czasownik"),
-	
+
 	WORD_VERB_IRREGULAR("czasownik nieregularny"),
-	
+
 	WORD_ADJECTIVE_I("i-przymiotnik"),
-	
+
 	WORD_ADJECTIVE_NA("na-przymiotnik"),
-	
+
 	WORD_KANJI_READING("kanji czytanie"),
-	
+
 	WORD_EXPRESSION("Wyrażenia"),
-	
+
 	WORD_THAT_POINT("Wskazywanie punktu"),
-	
+
 	WORD_ASK("Słówka pytające"),
-	
+
 	WORD_ADVERB("Przysłówek"),
-	
+
 	WORD_DAY_NUMBER("Numer dnia"),
-	
+
 	WORD_DAY_WEEK("Dzień tygodnia"),
-	
+
 	WORD_MONTH("Miesiąc"),
-	
+
 	WORD_LOCATION("Lokalizacja"),
-	
+
 	WORD_PEOPLE_NUMBER("Liczenie ludzi"),
-	
+
 	WORD_COUNT_DAY_NUMBER("Liczenie dni"),
-	
+
 	WORD_COUNTERS("Klasyfikatory"),
-	
+
 	WORD_PRONOUN("Zaimki"),
-	
+
 	WORD_NAME("Imię"),
-	
+
 	WORD_MALE_NAME("Imię męskie"),
-	
+
 	WORD_FEMALE_NAME("Imię żeńskie"),
-	
+
 	WORD_SURNAME_NAME("Nazwisko"),
-	
+
+	WORD_CONJUNCTION("Spójnik"),
+
+	WORD_PARTICULE("Partykuła"),
+
+	WORD_INTERJECTION("Wykrzyknik"),
+
 	UNKNOWN("unknown");
-	
+
 	private String name;
-	
+
 	DictionaryEntryType(String name) {
 		this.name = name;
 	}
