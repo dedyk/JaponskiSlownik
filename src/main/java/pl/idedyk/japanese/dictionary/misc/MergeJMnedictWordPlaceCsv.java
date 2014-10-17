28aaa016f1d897803953536e4750f14f4281e97d
dedyk <dedykjp@gmail.com>
2014-10-17 23:27:06 +0200

    update

---
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/MergeJMnedictWordPlaceCsv.java
@@ -0,0 +1,42 @@
+package pl.idedyk.japanese.dictionary.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import pl.idedyk.japanese.dictionary.dto.PolishJapaneseEntry;
+import pl.idedyk.japanese.dictionary.tools.CsvReaderWriter;
+
+public class MergeJMnedictWordPlaceCsv {
+
+	public static void main(String[] args) throws Exception {
+				
+		List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names/WORD_PLACE-oczekujace.csv");
+		
+		Map<Integer, PolishJapaneseEntry> waitingWordPlaceMap = new TreeMap<Integer, PolishJapaneseEntry>();
+		
+		for (PolishJapaneseEntry currentWaitingPolishJapaneseEntry : waitingWordPlaceList) {
+			waitingWordPlaceMap.put(currentWaitingPolishJapaneseEntry.getId(), currentWaitingPolishJapaneseEntry);
+		}
+		
+		List<PolishJapaneseEntry> wordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names/WORD_PLACE.csv");
+		
+		List<PolishJapaneseEntry> newWordPlaceList = new ArrayList<PolishJapaneseEntry>();
+		
+		for (PolishJapaneseEntry currentPolishJapaneseEntry : wordPlaceList) {
+			
+			PolishJapaneseEntry waitingPolishJapaneseEntry = waitingWordPlaceMap.get(currentPolishJapaneseEntry.getId());
+			
+			if (waitingPolishJapaneseEntry != null) {
+				newWordPlaceList.add(waitingPolishJapaneseEntry);
+				
+			} else {
+				newWordPlaceList.add(currentPolishJapaneseEntry);
+				
+			}
+		}
+		
+		CsvReaderWriter.generateCsv("input_names/WORD_PLACE.csv", newWordPlaceList, false);
+	}
+}
