7f9aa914bdac64fda285cc645daf6a003602b37a
dedyk <dedykjp@gmail.com>
2014-10-19 20:56:35 +0200

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
@@ -13,7 +13,8 @@ public class Split2JMnedictWordPlaceCsv {
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)\\ \\(.*\\)$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)*\\ \\(.*\\)$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-| )*\\(.*\\)$";
-		String matchTemplate = ".*\\(fabryka\\)$";
+		String matchTemplate = ".* de .*";
+		
 				
 		List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names/WORD_PLACE-oczekujace.csv");
 		
