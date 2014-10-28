e4d28f538bafbdc62bc827475a9b6defd6374b37
dedyk <dedykjp@gmail.com>
2014-10-28 13:09:47 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
@@ -28,7 +28,8 @@ public class Split2JMnedictWordPlaceCsv {
 		//String matchTemplate = ".*Peninsula.*";
 		//String matchTemplate = ".*Poland.*";
 		//String matchTemplate = ".*France.*";
-		String matchTemplate = ".*Russia\\)$";
+		//String matchTemplate = ".*Russia\\)$";
+		String matchTemplate = ".*\\(Australia\\)$";
 		
 		List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names/miss1/WORD_PLACE-oczekujace.csv");
 		//List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names2/WORD_PLACE.csv");
