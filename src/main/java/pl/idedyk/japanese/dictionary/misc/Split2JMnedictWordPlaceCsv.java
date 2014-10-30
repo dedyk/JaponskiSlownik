51df6bc401106c7342bedccaf9432282b56f1381
dedyk <dedykjp@gmail.com>
2014-10-30 21:34:46 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
@@ -37,7 +37,8 @@ public class Split2JMnedictWordPlaceCsv {
 		//String matchTemplate = ".*\\(.*\\)$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)*$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)*\\ \\(([A-Z]|[a-z]|'|-)*\\)$";
-		String matchTemplate = "^([A-Z]|[a-z]|)*\\ ([A-Z]|[a-z]|)*$";
+		//String matchTemplate = "^([A-Z]|[a-z]|)*\\ ([A-Z]|[a-z]|)*$";
+		String matchTemplate = "^.* von .*$";
 		
 		List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names/miss3/WORD_UNCLASS_NAME-oczekujace.csv");
 		//List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names2/WORD_PLACE.csv");
