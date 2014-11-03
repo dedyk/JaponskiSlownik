a9dc2feb3651e9c75db14c6225f61d3fc6f01002
dedyk <dedykjp@gmail.com>
2014-11-03 19:32:13 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/SplitJMnedict.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/SplitJMnedict.java
@@ -45,7 +45,7 @@ public class SplitJMnedict {
 			
 			List<PolishJapaneseEntry> dictionaryEntryTypeList = groupedPolishJapaneseEntryList.get(dictionaryEntryType);
 			
-			CsvReaderWriter.generateCsv("input_names2/" + dictionaryEntryType + ".csv", dictionaryEntryTypeList, false);			
+			CsvReaderWriter.generateCsv("input_names3/" + dictionaryEntryType + ".csv", dictionaryEntryTypeList, false);			
 		}		
 	}
 }
