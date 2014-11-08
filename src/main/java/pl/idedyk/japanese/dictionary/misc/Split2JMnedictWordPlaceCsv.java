8ce6a5326cecd15e64054d40d967f2bdc288b95b
dedyk <dedykjp@gmail.com>
2014-11-08 12:31:31 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
@@ -47,7 +47,8 @@ public class Split2JMnedictWordPlaceCsv {
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-| )* Institute$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-| )* Association$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
-		String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
+		//String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
+		String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
 		
 		List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names/miss4/WORD_ORGANIZATION_NAME-oczekujace.csv");
 		//List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names2/WORD_PLACE.csv");
