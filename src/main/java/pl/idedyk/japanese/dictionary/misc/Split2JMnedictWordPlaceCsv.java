ea0363d214d047cbf96cc7707947297dc79baa92
dedyk <dedykjp@gmail.com>
2014-11-08 12:36:26 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/Split2JMnedictWordPlaceCsv.java
@@ -49,7 +49,8 @@ public class Split2JMnedictWordPlaceCsv {
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
 		//String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
-		String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
+		//String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
+		String matchTemplate = "^([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)* ([A-Z]|[a-z]|'|-)*$";
 		
 		List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names/miss4/WORD_ORGANIZATION_NAME-oczekujace.csv");
 		//List<PolishJapaneseEntry> waitingWordPlaceList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names2/WORD_PLACE.csv");
