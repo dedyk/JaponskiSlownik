a3d129d33138f09af4d5165338101c5b276e8a3f
dedyk <dedykjp@gmail.com>
2014-12-18 17:13:32 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/common/Validator.java
+++ src/main/java/pl/idedyk/japanese/dictionary/common/Validator.java
@@ -370,7 +370,8 @@ public class Validator {
 				
 			}
 			
-			/*
+			
+			
 			for (PolishJapaneseEntry currentPolishJapaneseEntry : polishJapaneseKanjiEntries) {
 				
 				List<DictionaryEntryType> dictionaryEntryTypeList = currentPolishJapaneseEntry.getDictionaryEntryTypeList();
@@ -379,11 +380,15 @@ public class Validator {
 				
 				if (list != null) {
 					
+					list = new ArrayList<DictionaryEntryType>(new LinkedHashSet<DictionaryEntryType>(list));
+					
 					if (dictionaryEntryTypeList.size() == 1 && dictionaryEntryTypeList.get(0) == DictionaryEntryType.WORD_NOUN) {
 						
 						if (list.size() == 2 && list.get(0) == DictionaryEntryType.WORD_NOUN && list.get(1) == DictionaryEntryType.WORD_ADJECTIVE_NO) {
 							
 							currentPolishJapaneseEntry.setDictionaryEntryTypeList(list);
+							
+							System.out.println("AAAAA");
 						}
 						
 						
@@ -397,13 +402,14 @@ public class Validator {
 					
 				}				
 			}
-			*/
+			
+			
 			
 			try {
 				
-				//CsvReaderWriter.generateCsv("input/word-new2.csv", polishJapaneseKanjiEntries, true, true, false);
+				CsvReaderWriter.generateCsv("input/word-new2.csv", polishJapaneseKanjiEntries, true, true, false);
 				
-				
+				/*
 				CsvReaderWriter.generateCsv("input/word-new2.csv", polishJapaneseKanjiEntries, true, true, false,
 						new ICustomAdditionalCsvWriter() {
 		
@@ -453,6 +459,8 @@ public class Validator {
 					
 					
 				);
+				*/
+				
 				
 
 			} catch (Exception e) {
