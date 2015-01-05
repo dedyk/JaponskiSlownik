270d342c27acb1dbd9fc2ec88ec717f0fd35464e
dedyk <dedykjp@gmail.com>
2015-01-05 12:12:27 +0100

    testy

--- src/test/java/pl/idedyk/japanese/dictionary/test/Test4.java
+++ src/test/java/pl/idedyk/japanese/dictionary/test/Test4.java
@@ -1,13 +1,14 @@
 package pl.idedyk.japanese.dictionary.test;
 
-import java.util.List;
-import java.util.TreeMap;
+import java.util.Map;
 
-import pl.idedyk.japanese.dictionary.common.Helper;
-import pl.idedyk.japanese.dictionary.dto.JMEDictEntry;
-import pl.idedyk.japanese.dictionary.dto.PolishJapaneseEntry;
-import pl.idedyk.japanese.dictionary.tools.CsvReaderWriter;
-import pl.idedyk.japanese.dictionary.tools.JMEDictReader;
+import org.atilika.kuromoji.Token;
+import org.atilika.kuromoji.Tokenizer;
+import org.atilika.kuromoji.Tokenizer.Mode;
+
+import pl.idedyk.japanese.dictionary.api.dto.KanaEntry;
+import pl.idedyk.japanese.dictionary.api.tools.KanaHelper;
+import pl.idedyk.japanese.dictionary.api.tools.KanaHelper.KanaWord;
 
 public class Test4 {
 
@@ -62,11 +63,13 @@ public class Test4 {
 		unclass * ?
 		*/
 		
+		/*
 		TreeMap<String, List<JMEDictEntry>> jmedictName = JMEDictReader.readJMnedict("../JapaneseDictionary_additional/JMnedict-TEST.xml");
 		
 		List<PolishJapaneseEntry> generatedNames = Helper.generateNames(jmedictName);
 		
 		CsvReaderWriter.generateCsv("input_names3/test.csv", generatedNames, false);
+		*/
 		
 		/*
 		List<PolishJapaneseEntry> namesList = CsvReaderWriter.parsePolishJapaneseEntriesFromCsv("input_names2/names.csv");
@@ -89,5 +92,68 @@ public class Test4 {
 		
 		CsvReaderWriter.generateCsv("input_names2/names2.csv", smallNamesList, false);
 		*/
+		
+		// test tokenizera
+		//LuceneAnalyzer analyzerTest = new LuceneAnalyzer(Version.LUCENE_47);
+		//JapaneseAnalyzer analyzerTest = new JapaneseAnalyzer(Version.LUCENE_47);
+		
+		//String text = "1";
+		//String text = "1 ala";
+		//String text = "Ala ma kota i psa";
+		//String text = "Ala ma kota i psa2 i abc";
+		//String text = "dziesięć lat";
+		//String text = "お早う御座います";
+		
+		KanaHelper kanaHelper = new KanaHelper();
+				
+		Map<String, KanaEntry> kanaCache = kanaHelper.getKanaCache();
+		
+		String text = "北海道医療大学駅"; // hokkaidou iryou daigaku eki
+		
+		//String text = "小さくて奥ゆかしい日本語ヘルパー";
+		
+		//String text = "北朝鮮兵が中国で殺人韓国紙";
+		
+		//String text = "北朝鮮難民救援基金";
+		
+		Tokenizer tokenizer = Tokenizer.builder().mode(Mode.SEARCH).build();
+				
+        for (Token token : tokenizer.tokenize(text)) {
+        	
+        	String surfaceForm = token.getSurfaceForm();
+        	String reading = token.getReading();
+        	
+        	KanaWord kanaWord = kanaHelper.convertKanaStringIntoKanaWord(reading, kanaCache, false);
+        	
+        	String romaji = kanaHelper.createRomajiString(kanaWord);
+        	
+            System.out.println(surfaceForm + "\t" + reading + "\t" + romaji);
+        }
+		
+		/*
+		TokenStream tokenStream = analyzerTest.tokenStream("a", text);
+		
+		tokenStream.reset();
+		
+		while(true) {
+			
+			boolean incrementTokenResult = tokenStream.incrementToken();
+			
+			if (incrementTokenResult == false) {
+				break;
+			}
+			
+			System.out.println("Token: " + tokenStream.getAttribute(CharTermAttribute.class).toString());	
+			
+			ReadingAttribute readingAttribute = tokenStream.getAttribute(ReadingAttribute.class);
+			
+			System.out.println("R: " + readingAttribute.getPronunciation() + " - " + readingAttribute.getReading());
+			
+		}
+		
+		analyzerTest.close();
+		*/
+		
+		
 	}
 }
