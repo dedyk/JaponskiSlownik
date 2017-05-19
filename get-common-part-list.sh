51659b9fb9324446ef52f49ee1d8f9afa7d50a8a
dedyk <dedykjp@gmail.com>
2017-05-19 09:06:41 +0200

    update

--- get-common-part-list.sh
+++ get-common-part-list.sh
@@ -3,4 +3,6 @@
 . common.sh
 
 #cat input/common_word.csv | head -10000 | egrep -E -e "^[0-9]*,," | cut -d, -f1 | shuf | head -25 | sort -n
+#cat input/common_word.csv | head -10000 | egrep -E -e "^[0-9]*,," | cut -d, -f1 | wc -l
+
 java -Xmx4096m -cp $CLASSPATH:target/JapaneseDictionary-1.0-SNAPSHOT.jar pl.idedyk.japanese.dictionary.tools.wordgenerator.WordGenerator get-common-part-list $1
