de387a4e1b2229403b14f9c50fc0ae2f93eaa304
dedyk <dedykjp@gmail.com>
2018-11-27 12:30:59 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/tools/wordgenerator/WordGenerator.java
+++ src/main/java/pl/idedyk/japanese/dictionary/tools/wordgenerator/WordGenerator.java
@@ -2197,7 +2197,7 @@ public class WordGenerator {
 				}
 				
 				// stworzenie watkow
-				GeneratePrefixWordListThread[] generatePrefixWordListThreads = new GeneratePrefixWordListThread[2];
+				GeneratePrefixWordListThread[] generatePrefixWordListThreads = new GeneratePrefixWordListThread[4];
 				
 				for (int idx = 0; idx < generatePrefixWordListThreads.length; ++idx) {
 					generatePrefixWordListThreads[idx] = new GeneratePrefixWordListThread();
