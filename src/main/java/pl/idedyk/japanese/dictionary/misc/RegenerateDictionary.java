69f377d67db70959733860a82b6995b40a01f2b9
dedyk <dedykjp@gmail.com>
2019-09-28 09:46:54 +0200

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/RegenerateDictionary.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/RegenerateDictionary.java
@@ -2,6 +2,8 @@ package pl.idedyk.japanese.dictionary.misc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import pl.idedyk.japanese.dictionary.common.Helper;
 import pl.idedyk.japanese.dictionary.dto.PolishJapaneseEntry;
@@ -17,6 +19,7 @@ public class RegenerateDictionary {
 
 		polishJapaneseEntries = Helper.generateGroups(polishJapaneseEntries, false);
 
+		/*
 		List<PolishJapaneseEntry> resultPolishJapaneseEntries = new ArrayList<PolishJapaneseEntry>();
 
 		int id = 1;
@@ -29,8 +32,45 @@ public class RegenerateDictionary {
 
 			id++;
 		}
+		*/
+		
+		Map<String, List<PolishJapaneseEntry>> kanjiCache = new TreeMap<>();
+		Map<String, List<PolishJapaneseEntry>> kanaCache = new TreeMap<>();
+		
+		// generowanie cache'u z kanji jako kluczem
+		for (PolishJapaneseEntry currentPolishJapaneseEntry : polishJapaneseEntries) {
+			
+			// kanji cache
+			if (currentPolishJapaneseEntry.isKanjiExists() == true) {
+				
+				List<PolishJapaneseEntry> listForKanji = kanjiCache.get(currentPolishJapaneseEntry.getKanji());
+				
+				if (listForKanji == null) {
+					
+					listForKanji = new ArrayList<>();
+					
+					kanjiCache.put(currentPolishJapaneseEntry.getKanji(), listForKanji);
+				}
+				
+				listForKanji.add(currentPolishJapaneseEntry);				
+			}
+			
+			// kana cache
+			List<PolishJapaneseEntry> listForKana = kanaCache.get(currentPolishJapaneseEntry.getKana());
+			
+			if (listForKana == null) {
+				
+				listForKana = new ArrayList<>();
+				
+				kanaCache.put(currentPolishJapaneseEntry.getKana(), listForKana);
+			}
+			
+			listForKana.add(currentPolishJapaneseEntry);			
+		}
+		
+		//
 
-		for (PolishJapaneseEntry currentPolishJapaneseEntry : resultPolishJapaneseEntries) {
+		for (PolishJapaneseEntry currentPolishJapaneseEntry : polishJapaneseEntries) {
 
 			System.out.println(currentPolishJapaneseEntry.getId());
 			
@@ -47,25 +87,27 @@ public class RegenerateDictionary {
 				}				
 			}
 			
-			generateKnownDuplicatedIdForKanji(newKnownDuplicatedList, polishJapaneseEntries,
+			generateKnownDuplicatedIdForKanji(newKnownDuplicatedList, kanjiCache,
 					currentPolishJapaneseEntry.getId(), currentPolishJapaneseEntry.getKanji());
 
-			generateKnownDuplicatedIdFormKanjiAndKana(newKnownDuplicatedList, polishJapaneseEntries,
+			generateKnownDuplicatedIdFormKanjiAndKana(newKnownDuplicatedList, kanaCache,
 					currentPolishJapaneseEntry.getId(), currentPolishJapaneseEntry.getKanji(), kana);
-
+			
 			currentPolishJapaneseEntry.setKnownDuplicatedList(newKnownDuplicatedList);
 		}
 
-		CsvReaderWriter.generateCsv(new String[] { "input/word01-wynik.csv", "input/word02-wynik.csv", "input/word03-wynik.csv" }, resultPolishJapaneseEntries, true, true, false, true, null);
+		CsvReaderWriter.generateCsv(new String[] { "input/word01-wynik.csv", "input/word02-wynik.csv", "input/word03-wynik.csv" }, polishJapaneseEntries, true, true, false, true, null);
 	}
 
 	private static List<KnownDuplicate> generateKnownDuplicatedIdForKanji(List<KnownDuplicate> result,
-			List<PolishJapaneseEntry> polishJapaneseKanjiEntries,
+			Map<String, List<PolishJapaneseEntry>> kanjiCache,
 			int id, String kanji) {
 
 		if (kanji.equals("-") == true) {
 			return result;
 		}
+		
+		List<PolishJapaneseEntry> polishJapaneseKanjiEntries = kanjiCache.get(kanji);
 
 		for (PolishJapaneseEntry polishJapaneseEntry : polishJapaneseKanjiEntries) {
 
@@ -83,7 +125,9 @@ public class RegenerateDictionary {
 	}
 
 	private static void generateKnownDuplicatedIdFormKanjiAndKana(List<KnownDuplicate> result,
-			List<PolishJapaneseEntry> polishJapaneseKanjiEntries, int id, String kanji, String kana) {
+			Map<String, List<PolishJapaneseEntry>> kanaCache, int id, String kanji, String kana) {
+		
+		List<PolishJapaneseEntry> polishJapaneseKanjiEntries = kanaCache.get(kana);
 
 		for (PolishJapaneseEntry polishJapaneseEntry : polishJapaneseKanjiEntries) {
 
