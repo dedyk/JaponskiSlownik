616c4182a23181809068f07cbb7ec94c74af478a
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2021-03-25 19:20:27 +0100

    update

--- get-common-part-list.sh
+++ get-common-part-list.sh
@@ -8,5 +8,6 @@
 # cat input/kanji.csv | grep "Kanji: " | wc -l
 # kanji: =DŁ(H1)
 # cat input/common_word.csv | head -99999999 | egrep -E -e "^[0-9]*,," | cut -d, -f4 | shuf | head -10
+# cat input/word-new-test.csv | grep BEGIN | egrep -E -e "^BEGIN,[0-9]*,," | shuf | head -1
 
 java -Xmx4096m -cp $CLASSPATH:target/JapaneseDictionary-1.0-SNAPSHOT.jar pl.idedyk.japanese.dictionary.tools.wordgenerator.WordGenerator get-common-part-list $@
