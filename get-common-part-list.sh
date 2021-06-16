2527b336035ea9890af4f6075fe3a7bf292d69df
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2021-06-16 20:28:11 +0200

    update

--- get-common-part-list.sh
+++ get-common-part-list.sh
@@ -9,5 +9,6 @@
 # kanji: =DŁ(H1)
 # cat input/common_word.csv | head -99999999 | egrep -E -e "^[0-9]*,," | cut -d, -f4 | shuf | head -10
 # cat input/word-new-test.csv | egrep -E -e "^[0-9]*,BEGIN," | shuf | head -1
+# export LC_CTYPE=ja_JP.UTF-8
 
 java -Xmx4096m -cp $CLASSPATH:target/JapaneseDictionary-1.0-SNAPSHOT.jar pl.idedyk.japanese.dictionary.tools.wordgenerator.WordGenerator get-common-part-list $@
