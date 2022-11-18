263f4da8b6e4d5ac02ced17361dddfdc34be939f
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2022-11-18 21:04:30 +0100

    update

--- get-common-part-list.sh
+++ get-common-part-list.sh
@@ -11,6 +11,6 @@
 # cat input/word-new-test.csv | egrep -E -e "^[0-9]*,BEGIN," | shuf | head -1
 # export LC_CTYPE=ja_JP.UTF-8
 # paste -d'\n' plik1 plik2 | egrep -v '^$'
-# cat input/all_missing_word_from_group_id_in_dictionary2_format.csv | cut -c1-8
+# cat input/all_missing_word_from_group_id_in_dictionary2_format.csv | cut -c1-8 | sort | uniq
 
 java -Xmx4096m -cp $CLASSPATH:target/JapaneseDictionary-1.0-SNAPSHOT.jar pl.idedyk.japanese.dictionary.tools.wordgenerator.WordGenerator get-common-part-list $@
