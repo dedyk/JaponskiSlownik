a99bcb81663ce67f0f1207d352e755fc5d1df666
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2024-02-26 18:30:44 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/misc/ShowTheBiggerMissingJMdictEntries.java
+++ src/main/java/pl/idedyk/japanese/dictionary/misc/ShowTheBiggerMissingJMdictEntries.java
@@ -3,6 +3,7 @@ package pl.idedyk.japanese.dictionary.misc;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,7 +13,8 @@ import pl.idedyk.japanese.dictionary2.jmdict.xsd.JMdict.Entry;
 
 public class ShowTheBiggerMissingJMdictEntries {
 
-	private static final int MIN_SENSE_SIZE = 3;
+	private static final int MIN_SENSE_SIZE = 2;
+	private static final int[] RATIO = { 3, 7}; // proporcje duze do malych
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -21,6 +23,7 @@ public class ShowTheBiggerMissingJMdictEntries {
 		JMdict jmdict = dictionaryHelper.getJMdict();
 		List<JMdict.Entry> entryList = jmdict.getEntryList();
 
+		// lista wzystkich odnalezionych wpisow
 		List<JMdict.Entry> entryListBig = new ArrayList<>();
 		
 		for (JMdict.Entry entry : entryList) {
@@ -31,9 +34,29 @@ public class ShowTheBiggerMissingJMdictEntries {
 			}
 		}
 		
-		//
+		// posortowanie w kolejnosci po entryId
+		LinkedList<JMdict.Entry> entryListBigOrderByEntryId = new LinkedList<>(entryListBig);
 		
-		Collections.sort(entryListBig, new Comparator<JMdict.Entry>() {
+		Collections.sort(entryListBigOrderByEntryId, new Comparator<JMdict.Entry>() {
+
+			@Override
+			public int compare(JMdict.Entry o1, JMdict.Entry o2) {
+				if (o1.getSenseList().size() > o2.getSenseList().size()) {
+					return -1;
+					
+				} else if (o1.getSenseList().size() < o2.getSenseList().size()) {
+					return 1;
+					
+				} else {					
+					return o1.getEntryId().compareTo(o2.getEntryId());
+				}
+			}
+		});
+		
+		// posortowanie w kolejnosci od najwiekszych slowek
+		LinkedList<JMdict.Entry> entryListBigOrderByTheBiggest = new LinkedList<>(entryListBig);
+				
+		Collections.sort(entryListBigOrderByTheBiggest, new Comparator<JMdict.Entry>() {
 
 			@Override
 			public int compare(JMdict.Entry o1, JMdict.Entry o2) {
@@ -59,13 +82,49 @@ public class ShowTheBiggerMissingJMdictEntries {
 					} else {
 						return o1.getEntryId().compareTo(o2.getEntryId());
 					}
-					
-					// return o1.getEntryId().compareTo(o2.getEntryId());
 				}
 			}
 		});
 		
-		for (Entry entry : entryListBig) {
+		// wygenerowanie kolejek
+		
+		// wygenerowanie listy wedlug podanych proporcji
+		List<JMdict.Entry> entryListBigOrderByRatio = new ArrayList<>();
+		
+		BEFORE_WHILE:
+		while (true) {
+			// pobieramy duze wpisy
+			for (int bigIdx = 0; bigIdx < RATIO[0]; ++bigIdx) {
+				// pobieramy duzy wpis
+				JMdict.Entry entry = entryListBigOrderByTheBiggest.poll();
+				
+				if (entry == null) {
+					break BEFORE_WHILE;
+				}
+				
+				// dodajemy do listy wynikowej i usuwamy z malych slowek
+				entryListBigOrderByRatio.add(entry);
+				entryListBigOrderByEntryId.remove(entry);
+			}
+			
+			// pobieramy male wpisy
+			for (int bigIdx = 0; bigIdx < RATIO[1]; ++bigIdx) {
+				// pobieramy duzy wpis
+				JMdict.Entry entry = entryListBigOrderByEntryId.poll();
+				
+				if (entry == null) {
+					break BEFORE_WHILE;
+				}
+				
+				// dodajemy do listy wynikowej i usuwamy z malych slowek
+				entryListBigOrderByRatio.add(entry);
+				entryListBigOrderByTheBiggest.remove(entry);
+			}
+		}
+
+		//
+				
+		for (Entry entry : entryListBigOrderByRatio) {
 			System.out.println(entry.getEntryId());
 		}
 	}
