57340d7926714019c3f30c70573e574b41b44852
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2026-05-25 18:09:55 +0200

    Pdf dictionary update test

--- src/main/java/pl/idedyk/japanese/dictionary/tools/LatexDictionaryGenerator.java
+++ src/main/java/pl/idedyk/japanese/dictionary/tools/LatexDictionaryGenerator.java
@@ -56,7 +56,7 @@ public class LatexDictionaryGenerator {
 		// FM_FIXME: mala czesc
 		JMdict testPolishJMdict = new JMdict();
 		
-		testPolishJMdict.getEntryList().addAll(polishJMdict.getEntryList().subList(0, 1000));
+		testPolishJMdict.getEntryList().addAll(polishJMdict.getEntryList()); //.subList(0, 1000));
 		
 		PolishJapaneseLatexContent latexDictonaryEntries = generateLatexDictonaryEntries(testPolishJMdict);
 		
@@ -242,59 +242,41 @@ public class LatexDictionaryGenerator {
 			}
 			
 			latexContent.append("\\section*{" + section + "}\n");
-			latexContent.append("\\begin{multicols}{2}\n");
+			latexContent.append("\\noindent");				
+			latexContent.append("\\footnotesize\n");
+
+			//latexContent.append("\\begin{multicols}{2}\n");
 			
 			for (Entry<KanaRomajiKey, List<KanjiKanaPair>> currentSectionIndexEntry : indexSectionList) {
 				
 				// lista slow w danym miejscu
 				List<KanjiKanaPair> kanjiKanaPairList = currentSectionIndexEntry.getValue();
 				
-				latexContent.append("\\noindent\\begin{minipage}{\\linewidth}\n");
-				
-				latexContent.append("\\footnotesize\n");
 				// FM_FIXME: pogrubienie i upiekszenie
-				latexContent.append("\\textbf{" + currentSectionIndexEntry.getKey().romaji + "}, " + currentSectionIndexEntry.getKey().kana);
 				
-				if (kanjiKanaPairList.size() == 1) {
-					
-					if (kanjiKanaPairList.get(0).getKanjiInfo() != null) {
-						latexContent.append(", " + kanjiKanaPairList.get(0).getKanji());
-					}
-					
-					latexContent.append("\\dotfill");
-					latexContent.append("666");
-					latexContent.append("\\newline\n");					
-					
-				} else {
-					latexContent.append("\\newline\n");
-					latexContent.append("\\textbf{" + currentSectionIndexEntry.getKey().romaji + "}, \\textbf{" + currentSectionIndexEntry.getKey().kana + "}\\newline\n");
-				}
-				
+//				latexContent.append("\\dotfill");
+//				latexContent.append("666");
+//				latexContent.append("\\newline\n");					
+								
 				// latexContent.append("\\dotfill");
 				// latexContent.append("666");
-								
-				if (kanjiKanaPairList.size() > 1) {
 					
-					for (KanjiKanaPair kanjiKanaPair : kanjiKanaPairList) {
-						latexContent.append("\\hspace*{1.5em} ");
-						
-						// FM_FIXME: ulepszenia, np. pogrubienie
-						if (kanjiKanaPair.getKanjiInfo() != null) {
-							latexContent.append(kanjiKanaPair.getKanji());
-						} else {
-							latexContent.append(kanjiKanaPair.getKana());
-						}
-						
-						latexContent.append("\\dotfill");
-						latexContent.append("666");
-						
-						// FM_FIXME: \pageref{latex_test1}
-						
-						latexContent.append("\\newline\n");
+				for (KanjiKanaPair kanjiKanaPair : kanjiKanaPairList) {
+					latexContent.append("\\textbf{" + kanjiKanaPair.getRomaji() + "}, " + kanjiKanaPair.getKana());
+					
+					if (kanjiKanaPair.getKanjiInfo() != null) {
+						latexContent.append(", " + kanjiKanaPair.getKanji());
 					}
 					
-					// FM_FIXME: \pageref{latex_test1}					
+					//latexContent.append("\\dotfill");
+					latexContent.append("666");
+					
+					// FM_FIXME: \pageref{latex_test1}
+					
+					latexContent.append("\\newline\n");
 				}
+				
+				// FM_FIXME: \pageref{latex_test1}					
 				
 				
 				/*
@@ -303,13 +285,13 @@ public class LatexDictionaryGenerator {
 				\hspace*{1.5em} grawitacja \dotfill \pageref{latex_test3} \newline
 				\end{minipage}
 				*/
-				latexContent.append("\\end{minipage}\n");	
+				// latexContent.append("\\end{minipage}\n");	
 			}
 			
 			// FM_FIXME: \markboth !!!!
 			
 			
-			latexContent.append("\\end{multicols}\n");
+			//latexContent.append("\\end{multicols}\n");
 		}
 		
 		polishJapaneseLatexContent.japaneseIndex = latexContent.toString();
